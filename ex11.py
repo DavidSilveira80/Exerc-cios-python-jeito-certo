abe8783a4b9df851711751a03e0bfb3e9a3fa2c9
DavidSilveira80 <davidpscontato@gmail.com>
2022-04-13 17:06:36 -0300

    adicionado ex11.py Fazendo Perguntas

---
+++ ex11.py
@@ -0,0 +1,8 @@
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+heigth = input()
+print("How much do you weigh", end=' ')
+weigh = input()
+
+print(f"So, you're {age} old, {heigth} tall and {weigh} heavy.")
