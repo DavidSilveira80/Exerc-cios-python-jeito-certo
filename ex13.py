12d32551edabf925829e8c4302af059c68f2180f
DavidSilveira80 <davidpscontato@gmail.com>
2022-04-13 17:43:14 -0300

    adicionado ex13.py Parâmetros, Descompactação, Variáveis

---
+++ ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+# execute esse programa no terminal do seguinte modo: python3 ex13.py first 2nd 3nd 
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is", third)
