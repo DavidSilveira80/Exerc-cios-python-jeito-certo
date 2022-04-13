50e0a70c04900f7a312664a98af8beffc7ad7081
DavidSilveira80 <davidpscontato@gmail.com>
2022-04-13 15:08:59 -0300

    adicionado ex5.py Mais Variáveis e Impressão

---
+++ ex5.py
@@ -0,0 +1,18 @@
+my_name = 'Zed A. Shaw'
+my_age = 35 # não é mentira
+my_heigth = 74 # polegadas
+my_weigth = 180 # libras
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_heigth} inches tall.")
+print(f"He's {my_weigth} pounds heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffe.")
+
+# essa linha é capciosa, tente escrever exatamente como está
+total = my_age + my_heigth + my_weigth
+print(f"If I add {my_age}, {my_heigth}, and {my_weigth} I get {total}.")
